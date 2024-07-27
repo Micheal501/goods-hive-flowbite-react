3b83869ff7756888bceccf26324f6e7d00fec47f
Micheal Owen <mowen8983@gmail.com>
2024-07-27 16:01:12 +0700

    chore(assets): add product, brand, carousel, promo, category, guide images

---
+++ public/assets/img/images.jsx
@@ -0,0 +1,92 @@
+// hero carousel
+import mensHeroCarousel from "./hero carousel/mens.webp";
+import womensHeroCarousel from "./hero carousel/womens.webp";
+import shoesHeroCarousel from "./hero carousel/shoes.webp";
+
+// brands
+import nike from "./brands/nike.png";
+import puma from "./brands/puma.png";
+import longines from "./brands/longines.png";
+import prada from "./brands/prada.png";
+import rolex from "./brands/rolex.png";
+import chanel from "./brands/chanel.png";
+
+// category
+import mensCategory from "./category/mens.webp";
+import womensCategory from "./category/womens.webp";
+import fragrancesCategory from "./category/fragrances.webp";
+import sunglassesCategory from "./category/sunglasses.webp";
+
+// mens popular
+import mensPopularShirts from "./mens popular/shirts.webp";
+import mensPopularShoes from "./mens popular/shoes.webp";
+import mensPopularWatches from "./mens popular/watches.webp";
+
+// womens popular
+import womensPopularDresses from "./womens popular/dresses.webp";
+import womensPopularBags from "./womens popular/bags.webp";
+import womensPopularJewellery from "./womens popular/jewellery.webp";
+
+//  optional category carousel
+import mensOptionalCategoryCarousel from "./mens carousel/mens.png";
+import womensOptionalCategoryCarousel from "./womens carousel/womens-sale.png";
+import womensOptionalCategoryCarousel1 from "./womens carousel/womens-sale-1.png";
+
+// mens optional category
+import mensOptionalCategoryShirts from "./mens popular/shirts.webp";
+import mensOptionalCategoryShoes from "./mens popular/shoes.webp";
+import mensOptionalCategoryWatches from "./mens popular/watches.webp";
+
+// womens optional category
+import womensOptionalCategoryDresses from "./womens optional category/dresses.webp";
+import womensOptionalCategoryBags from "./womens optional category/bags.webp";
+import womensOptionalCategoryJewellery from "./womens optional category/jewellery.webp";
+import womensOptionalCategoryShoes from "./womens optional category/shoes.png";
+import womensOptionalCategoryBeauty from "./womens optional category/beauty.png";
+import womensOptionalCategoryWatches from "./womens optional category/watches.png";
+
+// this weeks promo
+import thisWeeksPromo from "./this weeks promo/this-week-promo.png";
+import thisWeeksPromo1 from "./this weeks promo/this-week-promo-1.png";
+
+// Shoes Guide
+import shoesGuide from "./guide/measure-shoes-size.png";
+
+const images = {
+  mensHeroCarousel,
+  womensHeroCarousel,
+  shoesHeroCarousel,
+  nike,
+  puma,
+  prada,
+  rolex,
+  longines,
+  chanel,
+  mensCategory,
+  womensCategory,
+  fragrancesCategory,
+  sunglassesCategory,
+  mensPopularShirts,
+  mensPopularShoes,
+  mensPopularWatches,
+  womensPopularDresses,
+  womensPopularBags,
+  womensPopularJewellery,
+  mensOptionalCategoryCarousel,
+  womensOptionalCategoryCarousel,
+  womensOptionalCategoryCarousel1,
+  mensOptionalCategoryShirts,
+  mensOptionalCategoryShoes,
+  mensOptionalCategoryWatches,
+  womensOptionalCategoryDresses,
+  womensOptionalCategoryBags,
+  womensOptionalCategoryJewellery,
+  womensOptionalCategoryShoes,
+  womensOptionalCategoryBeauty,
+  womensOptionalCategoryWatches,
+  thisWeeksPromo,
+  thisWeeksPromo1,
+  shoesGuide,
+};
+
+export default images;
