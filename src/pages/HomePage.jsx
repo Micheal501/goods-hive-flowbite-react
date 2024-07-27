8a6eaf2a29547934fe364790fb37db02fe1257e7
Micheal Owen <mowen8983@gmail.com>
2024-07-27 16:06:56 +0700

    feat: call Navbar, CarouselHero, MensPopular components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import CarouselHero from "../components/organisms/CarouselHero";
+import MensPopular from "../components/organisms/MensPopular";
+import NavBar from "../components/organisms/NavBar";
+const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+      <CarouselHero />
+      <MensPopular />
+    </>
+  );
+};
+
+export default HomePage;
