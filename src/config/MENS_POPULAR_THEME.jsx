6ee75bc2236bd9b3d6d1c10509778289335cf71f
Micheal Owen <mowen8983@gmail.com>
2024-07-27 16:05:44 +0700

    feat: add mens popular custom theme

---
+++ src/config/MENS_POPULAR_THEME.jsx
@@ -0,0 +1,11 @@
+export const MENS_POPULAR_THEME = {
+  root: {
+    base: "flex rounded-lg border border-gray-200 bg-white shadow-xl w-full mt-10 justify-center items-center hover:scale-110 ease-out duration-300",
+    children: "flex h-full flex-col justify-center gap-4",
+    horizontal: {
+      off: "flex-col",
+      on: "flex-col md:max-w-xl md:flex-row",
+    },
+    href: "hover:bg-gray-100 dark:hover:bg-gray-700",
+  },
+};
