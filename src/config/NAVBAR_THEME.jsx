f999b479b082e50420639cd04f90a2aa07130c50
Micheal Owen <mowen8983@gmail.com>
2024-07-27 16:06:13 +0700

    feat: add navigation bar custom theme

---
+++ src/config/NAVBAR_THEME.jsx
@@ -0,0 +1,14 @@
+export const navbarTheme = {
+  root: {
+    inner: {
+      base: "flex flex-wrap items-center justify-around",
+    },
+  },
+
+  link: {
+    active: {
+      on: "bg-[#BEA20B] text-white md:bg-transparent md:text-[#BEA20B]",
+      off: "border-b border-gray-100 text-gray-700 md:border-0 md:hover:bg-transparent md:hover:text-[#BEA20B]",
+    },
+  },
+};
