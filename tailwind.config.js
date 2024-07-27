19e65596fd8e735caa00f7cf4194a3b1fcd9ac56
Micheal Owen <mowen8983@gmail.com>
2024-07-27 13:57:12 +0700

    starter code

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    flowbite.content(),
+  ],
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
+};
